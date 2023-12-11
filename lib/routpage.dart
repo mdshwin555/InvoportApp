5a3370db6532a0a0b34aac1c6d2194ac6e288e81
mdshwin555 <shmouayad305@gmail.com>
2023-12-11 01:45:49 -0800

    version1.0.0

---
+++ lib/routpage.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'UI/screens/Home.dart';
+import 'UI/screens/login.dart';
+import 'constance/routes.dart';
+
+List<GetPage<dynamic>>? routes = [
+  // Auth
+  GetPage(name: AppRoute.signIn, page: () => const LogIn()),
+
+  // Home
+  GetPage(name: AppRoute.home, page: () =>  Home()),
+];
